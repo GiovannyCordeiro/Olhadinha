0d1475ea1d3e0ccdfade815b9e8a2b3fe7b3672d
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2023-07-08 16:33:46 -0300

    Refact: New dependencies and att .env-example

--- src/config.py
+++ src/config.py
@@ -9,6 +9,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = f"mysql://{os.environ['USERDB']}:{os.environ['PASSWORDDB']}@localhost/flask_mvc"
+SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2:://{os.environ['USERDB']}:{os.environ['PASSWORDDB']}@{os.environ['LOCALDB']}/{os.environ['NAMEDB']}"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
