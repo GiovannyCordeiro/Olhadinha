16fd0a0321b5060bf233a37b178ca0ceb60d170f
Victor Medeiros <117488402+ViktorMedeiros@users.noreply.github.com>
2023-07-09 20:56:16 -0300

    Update blueprint.py

--- src/routes/blueprint.py
+++ src/routes/blueprint.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
 from controllers.TestController import TestController
 
-blueprint = Blueprint('blueprint', __name__)
 
-blueprint.route('/', methods=['GET'])(TestController.index)
+gdosfasdodsa
