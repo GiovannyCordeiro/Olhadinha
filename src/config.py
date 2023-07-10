8d91ed5a94f55cf59a739fdae7b321d9cff88dd1
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2023-07-10 06:10:25 -0300

    Test: Testing pipeline

--- src/config.py
+++ src/config.py
@@ -9,6 +9,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ['USERDB']}:{os.environ['PASSWORDDB']}@{os.environ['LOCALDB']}/{os.environ['NAMEDB']}"
+# Dev
+# SQLALCHEMY_DATABASE_URI = f"postgresql://{os.environ['USERDB']}:{os.environ['PASSWORDDB']}@{os.environ['LOCALDB']}/{os.environ['NAMEDB']}"
+
+# prod
+SQLALCHEMY_DATABASE_URI = os.environ['DB_CONECTION']
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
