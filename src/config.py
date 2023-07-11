43d0ff621e317d1511dbeac403b5acbf64acbb1d
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2023-07-11 09:15:03 -0300

    refact

--- src/config.py
+++ src/config.py
@@ -6,6 +6,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = os.getenv('DBCONNECTION')
+# SQLALCHEMY_DATABASE_URI = os.getenv('DBCONNECTION')
+
+SQLALCHEMY_DATABASE_URI = '${{ secrets.DB_CONECTION }}'
+
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
