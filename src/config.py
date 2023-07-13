9817a5452a337f364e1bce8952e20cdd7229d2c9
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2023-07-13 17:48:40 -0300

    Feat: Testing with database its working

--- src/config.py
+++ src/config.py
@@ -9,6 +9,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = os.getenv('DBCONNECTION')
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@192.168.0.102:5433/public"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
