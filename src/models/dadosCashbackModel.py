bf91c641d813e4bf1fd72a092830a2738c0014fd
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2023-07-23 14:16:51 -0300

    Feat: Adding dadosCashbackModel

---
+++ src/models/dadosCashbackModel.py
@@ -0,0 +1,14 @@
+from models.base.StartDBModel import db
+from models.platformsModel import Plataformas
+from sqlalchemy import ForeignKey
+from sqlalchemy.sql import func
+
+class DadosCashback(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    porcentagem = db.Column(db.String)
+    plataforma = db.Column(db.Integer, ForeignKey(Plataformas.id))
+    loja = db.Column(db.String)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+
+    def __repr__(self):
+        return f"DadosCashback (id={self.id}, porcentagem={self.porcentagem})"
