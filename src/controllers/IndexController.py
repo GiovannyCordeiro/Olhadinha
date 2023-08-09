9760b890889e5c604128f89c788f1de5af33d640
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2023-08-09 15:59:23 -0300

    Feat: Adding response for root page

--- src/controllers/IndexController.py
+++ src/controllers/IndexController.py
@@ -1,10 +1,6 @@
-import json
-# from models.machine import db
-from services.first_service import TestService
-
 class IndexController:
     def index():
         return {
         'status': 'OK',
-        'data': TestService.index()
+        'data': "Welcome to OLHADINHA API"
         }
