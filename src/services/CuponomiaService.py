6676001be92627369bb5e439e694b8b1ec531852
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2023-08-26 19:27:24 -0300

    Refact: Capture data correctly

--- src/services/CuponomiaService.py
+++ src/services/CuponomiaService.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 class CuponomiaService:
-    def extract(store):
+    def extract(store:str):
         url = f"https://www.cuponomia.com.br/desconto/{store}"
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0"
@@ -10,5 +10,6 @@ class CuponomiaService:
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.text, 'html.parser')
         textElement = soup.find(class_="rewardsTag-cashback").text
-        cashbackPercentage = textElement[12:13]
+        cashbackPercentage = textElement[8:9]
+        print(cashbackPercentage)
         return int(cashbackPercentage)
