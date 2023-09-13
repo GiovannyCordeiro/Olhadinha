aa7c9b98da5c068a76ea394b9d9fd9d4849ef7af
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2023-09-13 11:10:28 -0300

    Feat: Adding logic in return for user

--- src/services/FiringPlatformService.py
+++ src/services/FiringPlatformService.py
@@ -1,10 +1,12 @@
 from services.CuponomiaService import CuponomiaService
 from services.ZoomService import ZoomService
 from services.InterShopService import InterShopService
+from services.MeuDimDimService import MeuDimDimService
 
 class FirePlatform:
     logic = {
         'cuponomia': lambda store: CuponomiaService.extract(store),
         'zoom': lambda store: ZoomService.extract(store),
-        'intershop': lambda store: InterShopService.extract(store)
+        'intershop': lambda store: InterShopService.extract(store),
+        'meudimdim': lambda store: MeuDimDimService.extract(store)
     }
