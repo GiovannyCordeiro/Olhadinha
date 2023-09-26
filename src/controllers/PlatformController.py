9de3f83970f99cad8de4d19e965252d4086c2fc9
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2023-09-26 09:47:59 -0300

    refact: removing route

--- src/controllers/PlatformController.py
+++ src/controllers/PlatformController.py
@@ -1,17 +1,17 @@
-from services.CuponomiaService import CuponomiaService
-from services.ZoomService import ZoomService
-from services.VerifyExistsDataService import VerifyExistsDataService
+# from services.CuponomiaService import CuponomiaService
+# from services.ZoomService import ZoomService
+# from services.VerifyExistsDataService import VerifyExistsDataService
 
-class PlatformController:
+# class PlatformController:
 
-    @classmethod
-    def searchPlatforms(cls,store):
-        return {
-            'status': 'OK',
-            'cashbackPlatfomrs': {
-                "Lorem ipsum": "DOLOR",
-                "test": f"{VerifyExistsDataService.search(store,'meliuz')}"
-                # 'Cuponomia': CuponomiaService.extract(store),
-                # 'Zoon': ZoomService.extract(store)
-            }
-        }
+#     @classmethod
+#     def searchPlatforms(cls,store):
+#         return {
+#             'status': 'OK',
+#             'cashbackPlatfomrs': {
+#                 "Lorem ipsum": "DOLOR",
+#                 "test": f"{VerifyExistsDataService.search(store,'meliuz')}"
+#                 # 'Cuponomia': CuponomiaService.extract(store),
+#                 # 'Zoon': ZoomService.extract(store)
+#             }
+#         }
--- src/routes/blueprint.py
+++ src/routes/blueprint.py
@@ -1,11 +1,10 @@
 from flask import Blueprint
 from controllers.IndexController import IndexController
-from controllers.PlatformController import PlatformController
+# from controllers.PlatformController import PlatformController
 
 from services.DataSearchIntermediaryService import DataSearchIntermediaryService
 
 blueprint = Blueprint('blueprint', __name__)
 
-blueprint.route('/<store>', methods=['GET'])(DataSearchIntermediaryService.consultCashbackData)
-blueprint.route('/platform/<store>', methods=['GET'])(PlatformController.searchPlatforms)
+blueprint.route('/platform/<store>', methods=['GET'])(DataSearchIntermediaryService.consultCashbackData)
 blueprint.route('/', methods=['GET'])(IndexController.index)
