48b0f31cf0b218bd4c9e6da625c1005bc9834383
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2023-10-26 09:59:11 -0300

    fix: making the front query the back in production

--- backend/src/controllers/IndexController.py
+++ backend/src/controllers/IndexController.py
@@ -1,6 +1,14 @@
+from flask import request, abort
+
 class IndexController:
     def index():
+        origin = request.headers.get("Origin") 
+        # if origin != "http://localhost:3050":
+        #     abort(403)
+        # if origin != "http://localhost:3000":
+        #     abort(403)
         return {
         'status': 'OK',
-        'data': "Welcome to OLHADINHA API"
+        'data': "Welcome to OLHADINHA API",
+        'origin': origin
         }
