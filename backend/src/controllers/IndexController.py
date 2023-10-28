406b4778f17d577fee1302146dc600158fbea367
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2023-10-28 16:21:41 -0300

    feat: Adding treatment requests

--- backend/src/controllers/IndexController.py
+++ backend/src/controllers/IndexController.py
@@ -1,12 +1,13 @@
 from flask import request, abort
+import os
 
 class IndexController:
     def index():
         origin = request.headers.get("Origin") 
-        # if origin != "http://localhost:3050":
-        #     abort(403)
-        # if origin != "http://localhost:3000":
-        #     abort(403)
+        if os.environ['ENVIRONMENT_VARIABLE'] == "PROD":
+            if origin != "http://localhost:3000":
+                abort(403)
+
         return {
         'status': 'OK',
         'data': "Welcome to OLHADINHA API",
