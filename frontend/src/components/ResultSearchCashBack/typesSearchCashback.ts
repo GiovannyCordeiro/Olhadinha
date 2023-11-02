73b2b1c1bd1163ffb73024af76e99ffa1844b6dd
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2023-11-02 19:11:13 -0300

    feat: defining interfaces for SearchCashBack component

---
+++ frontend/src/components/ResultSearchCashBack/typesSearchCashback.ts
@@ -0,0 +1,12 @@
+import { Params } from "react-router-dom"
+
+export interface ParamsProp {
+    params: Params,
+}
+
+export interface DataRootAPI {
+    cuponomia: string;
+    intershop: string;
+    meudimdim: string;
+    zoom: string;
+}
