343608090555bfeccbd33164b086a301472108d5
Giovanny Cordeiro <Giovannycordeiropb@gmail.com>
2023-11-15 18:00:35 -0300

    refact: specifying return method

--- backend/src/services/ZoomService.py
+++ backend/src/services/ZoomService.py
@@ -5,7 +5,7 @@ from helpers.Plataforms import indexPlatformsDB
 import re
 
 class ZoomService:
-    def extract(store: str):
+    def extract(store: str) -> str:
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0"
         }
