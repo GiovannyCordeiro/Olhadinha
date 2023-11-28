91b17a189afc38812ad9f162d7b9aeafe018323d
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2023-11-28 12:04:00 -0300

    refact: removing unnecessary code

--- backend/src/controllers/IndexController.py
+++ backend/src/controllers/IndexController.py
@@ -3,11 +3,6 @@ import os
 
 class IndexController:
     def index():
-        origin = request.headers.get("Origin") 
-        if os.environ['ENVIRONMENT_VARIABLE'] == "PROD":
-            if origin != "http://localhost:3000" or origin != "http://localhost:80":
-                abort(403)
-
         return {
         'status': 'OK',
         'data': "Welcome to OLHADINHA API"
