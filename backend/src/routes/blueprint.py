baf24c886ccf21763fe292b0a2db525c2bb34769
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2023-12-01 12:21:13 -0300

    fix: return another aproach

--- backend/src/routes/blueprint.py
+++ backend/src/routes/blueprint.py
@@ -5,5 +5,5 @@ from services.DataSearchIntermediaryService import DataSearchIntermediaryService
 
 blueprint = Blueprint('blueprint', __name__)
 
-blueprint.route('/api/platform/<store>', methods=['GET'])(DataSearchIntermediaryService.consultCashbackData)
-blueprint.route('/api/', methods=['GET'])(IndexController.index)
+blueprint.route('/platform/<store>', methods=['GET'])(DataSearchIntermediaryService.consultCashbackData)
+blueprint.route('/', methods=['GET'])(IndexController.index)
