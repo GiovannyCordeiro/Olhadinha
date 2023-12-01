de70c533134b75a2e7dd6ce4590b4c50d64ad6da
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2023-12-01 11:54:10 -0300

    build: testing with change code

--- bin/applicationEnvironment.sh
+++ bin/applicationEnvironment.sh
@@ -2,7 +2,13 @@
 
 l=$1
 
-if [[ $l = "prod" || $l = "dev" ]] && [[ $2 = "down" || $2 = "up" ]]; then
+if [[ $l = "prod" ]] && [[ $2 = "down" || $2 = "up" ]]; then
+    cd ..
+    fileEnv="docker-compose.$l.yml"
+    downOrUp=$2
+    echo "Running docker-compose -f docker-compose.yml -f $fileEnv $downOrUp"
+    docker-compose -f docker-compose.yml -f $fileEnv $downOrUp -d
+elif [[ $l = "dev" ]] && [[ $2 = "down" || $2 = "up" ]]; then
     cd ..
     fileEnv="docker-compose.$l.yml"
     downOrUp=$2
